6c768c374cf72021fe1f0e3aab0eaafd426f87d7
rookie <675611058@qq.com>
2021-02-04 22:01:40 +0800

    制作冲突1

--- src/main/java/com/hefeng/domain/User.java
+++ src/main/java/com/hefeng/domain/User.java
@@ -10,6 +10,8 @@ public class User implements Serializable {
 
     private String username;//姓名
 
+    private Integer age;
+
     public static void main(String[] args) {
         System.out.println("hello world");
     }
