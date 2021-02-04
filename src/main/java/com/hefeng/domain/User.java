67c7db19eeec8f9e3ed3de35e2dc7c70c8ab748b
rookie <675611058@qq.com>
2021-02-04 21:55:39 +0800

    第一次提交项目

---
+++ src/main/java/com/hefeng/domain/User.java
@@ -0,0 +1,16 @@
+package com.hefeng.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author rookie
+ * @date 2021/02/04 21:45
+ **/
+public class User implements Serializable {
+
+    private String username;//姓名
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
